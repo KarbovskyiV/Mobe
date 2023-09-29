1149b7f9826f15867310e911c4734a8e0b06d300
YanaVesna <kydves78@web.de>
2023-09-29 19:30:40 +0200

    29-09-23

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
+import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+export const CategoryContext = React.createContext();
 
 function App() {
+  const [categoryValue, setCategoryValue] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CategoryContext.Provider value={{ categoryValue, setCategoryValue }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+        <Footer />
+      </CategoryContext.Provider>
     </div>
   );
 }
