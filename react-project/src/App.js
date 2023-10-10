bce49b68259a1cf67391bf6b1909b45441adc673
Vasyl <vkoltsov316@gmail.com>
2023-10-10 18:53:07 +0300

    add slides for first pages

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer.jsx";
 import LoginPage from "./pages/Signin.jsx";
 import Registration from "./pages/Registration.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import Slider from "./components/Slider/Slider";
 
 export const CategoryContext = React.createContext();
 export const RegistrationActiveContext = React.createContext();
@@ -63,6 +64,7 @@ function App() {
                       value={{ categoryValue, setCategoryValue }}
                     >
                       <Header />
+                      <Slider />
                       <Routes>
                         <Route path="/NotFound" element={<NotFound />} />
                         <Route path="/" element={<Home />} />
