53ef35f4d71a798bc2169f9ed34b4ddb3737ee44
Volodymyr <101818157+KarbovskyiV@users.noreply.github.com>
2023-10-18 20:37:04 +0300

    Update axios.js

--- react-project/src/utils/axios.js
+++ react-project/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "127.0.0.1:81/api",
+  baseURL: "http://127.0.0.1:81/api",
   /* baseURL: "http://127.0.0.1:3306/api", */
 });
 
