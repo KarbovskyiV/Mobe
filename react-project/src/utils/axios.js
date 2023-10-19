d58da03e08494060baa2c4b4796e2904538bdf58
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-10-19 09:07:28 +0000

    change start react

--- react-project/src/utils/axios.js
+++ react-project/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:81/api",
+  baseURL: "http://mobe.publicvm.com:81/api",
   /* baseURL: "http://127.0.0.1:3306/api", */
 });
 
