b7b584d57b151ae3b62ad62cf6e5ecf622756446
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-10-31 21:30:21 +0200

    change reset password to according to our style

--- back-end/resources/views/emails/reset-password.blade.php
+++ back-end/resources/views/emails/reset-password.blade.php
@@ -3,45 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>Password Reset</title>
 </head>
-<body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0;">
-<table role="presentation" cellspacing="0" cellpadding="0" width="100%"
-       style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px;">
-    <tr>
-        <td style="text-align: center; padding: 10px 0;">
-            <h2 style="color: #333;">Password Reset</h2>
-        </td>
-    </tr>
-    <tr>
-        <td style="padding: 20px 0;">
-            <p style="font-size: 16px; line-height: 1.6; color: #333;">
-                You've requested to reset your password. Click the button below to reset it:
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td style="text-align: center; padding: 20px 0;">
-            <a href="{{ $resetLink }}"
-               style="display: inline-block; background-color: #007BFF; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold; font-size: 16px;">Reset
-                Password</a>
-        </td>
-    </tr>
-    <tr>
-        <td style="padding: 20px 0;">
-            <p style="font-size: 16px; line-height: 1.6; color: #333;">
-                If you didn't request this, please ignore this email.
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td style="padding: 20px 0;">
-            <p style="font-size: 16px; line-height: 1.6; color: #333;">
-                Regards,<br>
-                Your Mobe Team
-            </p>
-        </td>
-    </tr>
-</table>
+<body>
+
+<div class="container">
+    <div class="header">
+        <img src="{{ asset('images/logo_mobe.svg') }}" alt="Logo" class="logo">
+        <h2 class="title">Password Reset</h2>
+    </div>
+    <div class="password-description">
+        <p>You've requested to reset your password. Click the button below to reset it:</p>
+    </div>
+    <div class="button-container">
+        <a href="#" class="reset-button">Reset Password</a>
+    </div>
+    <div class="disclaimer">
+        <p>If you didn't request this, please ignore this email.</p>
+    </div>
+    <div class="signature">
+        <p>Regards,<br>Your Mobe Team</p>
+    </div>
+</div>
+
 </body>
 </html>
