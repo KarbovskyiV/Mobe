5be370a1a6cfdbe3537728d772016b310d8be4c9
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-02 16:32:28 +0200

    move all styles to html file, becaouse of gmail

--- back-end/resources/views/emails/reset-password.blade.php
+++ back-end/resources/views/emails/reset-password.blade.php
@@ -9,6 +9,106 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>Password Reset</title>
+    <style>
+        body {
+            font-family: "Inter", sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        p {
+            font-weight: 400;
+            font-size: 16px;
+            margin: 0;
+        }
+
+        .container {
+            width: 600px;
+            height: 688px;
+            margin: 0 auto;
+            background-color: #ffffff;
+        }
+
+        .header {
+            text-align: center;
+        }
+
+        .logo {
+            margin-top: 100px;
+        }
+
+        .title {
+            font-family: 'Inter', sans-serif;
+            font-weight: 500;
+            color: #28003e;
+            font-size: 32px;
+            margin: 60px 0 0 0;
+        }
+
+        .password-description {
+            margin: 40px 40px 0 40px;
+        }
+
+        .button-container {
+            text-align: center;
+            margin-top: 60px;
+        }
+
+        .reset-button {
+            font-weight: 400;
+            background-color: #8732C9;
+            color: #FDFDFD;
+            width: 170px;
+            height: 44px;
+            text-decoration: none;
+            padding: 12px 24px;
+            border-radius: 3px;
+            font-size: 16px;
+            margin-top: 60px;
+        }
+
+        .disclaimer {
+            margin: 60px 40px 0 40px;
+        }
+
+        .signature {
+            margin: 40px 40px 0 40px;
+        }
+
+        @media (max-width: 375px) {
+            p {
+                font-size: 14px;
+            }
+
+            .container {
+                width: 280px;
+            }
+
+            .title {
+                font-size: 24px;
+            }
+
+            .password-description {
+                margin: 40px 20px 0 20px;
+            }
+
+            .button-container {
+                margin-top: 40px;
+            }
+
+            .disclaimer {
+                margin: 40px 20px 0 20px;
+            }
+
+            .signature {
+                margin: 40px 20px 0 20px;
+            }
+        }
+
+
+    </style>
+
 </head>
 <body>
 
