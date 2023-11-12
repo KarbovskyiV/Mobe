ad25dd371531369858d4127a20465bcadb455ecb
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-12 18:09:33 +0200

    adds verify

--- back-end/app/Http/Controllers/VerificationController.php
+++ back-end/app/Http/Controllers/VerificationController.php
@@ -4,33 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cookie;
 
 class VerificationController extends Controller
 {
     public function verify($id)
     {
         $user = User::find($id);
-        $token = $user->createToken('email-verification', ['expires_in' => 5])->plainTextToken;
 
         if ($user->hasVerifiedEmail()) {
-            return redirect('http://localhost:3000/')->with(['message' => 'Email already verified.', 'user' => $user,
-                'token' => $token,], 400);
+            return redirect('http://localhost:3000/')->with([
+                'message' => 'Email already verified.',
+                'verification' => false,
+                'email' => $user->email,
+            ], 400);
         }
 
         /** @var User $user */
         $user->markEmailAsVerified();
 
-        $token = $user->createToken('email-verification', ['expires_in' => 5])->plainTextToken;
-
         Auth::login($user);
 
-        // force login if token true on frontend
-
-        return redirect('http://localhost:3000/')->with([
+// redirect('http://localhost:3000/')
+        return response()->json([
             'message' => 'Email successfully verified.',
-            'user' => $user,
-            'token' => $token,
+            'verification' => true,
+            'email' => $user->email,
         ]);
+        // return verification true of false
     }
 }
