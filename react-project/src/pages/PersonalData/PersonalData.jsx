1bc566afd750e11fb67b34818d828f3f8b6f8001
rodindima <rodindima1984@gmail.com>
2023-11-23 16:20:55 +0200

    add-page-personal-data

--- react-project/src/pages/PersonalData/PersonalData.jsx
+++ react-project/src/pages/PersonalData/PersonalData.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./style.scss";
 import AdminLink from "../../components/AdminLink/AdminLink";
+import Title from "../../components/Title/Title";
+import HotPriceContainer from "../../Containers/HotPrice/HotPriceContainer";
 
 const PersonalData = () => {
   return (
@@ -11,7 +13,29 @@ const PersonalData = () => {
           <div className="personal__admin">
             <AdminLink />
           </div>
+          <div className="personal__content">
+            <Title text="Personal data" />
+            <div className="personal__data">
+              <div className="personal__data-item">
+                <p className="personal__data-const">Name</p>
+                <p>Antonina</p>
+              </div>
+              <div className="personal__data-item">
+                <p className="personal__data-const">Surname</p>
+                <p>Synelnyk</p>
+              </div>
+              <div className="personal__data-item">
+                <p className="personal__data-const">Phone</p>
+                <p>+380 63 629 73 28</p>
+              </div>
+              <div className="personal__data-item">
+                <p className="personal__data-const">Email</p>
+                <p>antonina_luk@ukr.net</p>
+              </div>
+            </div>
+          </div>
         </div>
+        <HotPriceContainer />
       </section>
     </>
   );
