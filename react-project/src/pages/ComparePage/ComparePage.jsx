b9b9fbbb67b55a0ae946de3f8241a5813a14d690
rodindima <rodindima1984@gmail.com>
2023-11-26 15:17:35 +0300

    add-pulic-on-compare-wish

--- react-project/src/pages/ComparePage/ComparePage.jsx
+++ react-project/src/pages/ComparePage/ComparePage.jsx
@@ -27,7 +27,9 @@ const ComparePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://mobe.local.com/api/products");
+        const response = await axios.get(
+          "http://mobe.publicvm.com:81/api/products"
+        );
         if (Array.isArray(response.data)) {
           setAllProducts(response.data);
         } else {
